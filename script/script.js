40aeceb6f22e47b4a37e041b6910b56d6ec9d9a2
Ygor Scofano <cinzeiro@outlook.com.br>
2025-11-22 15:24:18 -0300

    background added and header modifications

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-console.log('No script on this website');
+let contact = document.querySelector('#footer');
+function scrollContact(contact) {
+    const itemHeader = document.querySelector('a[href="#footer"]');
+    itemHeader.addEventListener('click', (e) => {
+        e.preventDefault();
+        contact.scrollIntoView({ behavior: 'smooth' });
+    });
+};
+scrollContact(contact);
